a3468071c9e5a5d3407cebf8c03b510daab97479
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2026-02-19 01:40:55 +0530

    [CONFIG]: update harhat config for blockscout contract verification

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 const { CELO_RPC_URL, DEPLOYER_PRIVATE_KEY } = process.env;
@@ -7,14 +8,30 @@ module.exports = {
   solidity: {
     version: "0.8.24",
     settings: {
-      optimizer: { enabled: true, runs: 200 }
-    }
+      optimizer: { enabled: true, runs: 200 },
+    },
   },
   networks: {
     celoSepolia: {
       url: CELO_RPC_URL,
       accounts: [DEPLOYER_PRIVATE_KEY],
-      chainId: 11142220
-    }
-  }
+      chainId: 11142220,
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      celoSepolia: "empty", // Blockscout doesn't require a real key
+    },
+    customChains: [
+      {
+        network: "celoSepolia",
+        chainId: 11142220,
+        urls: {
+          apiURL: "https://celo-sepolia.blockscout.com/api",
+          browserURL: "https://celo-sepolia.blockscout.com",
+        },
+      },
+    ],
+  },
 };
